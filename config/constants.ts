905e5ac901e7859abe758ecac41a5f6e81b47012
= <>
2018-09-11 20:42:29 +0300

    hided API-keys hihi

--- config/constants.ts
+++ config/constants.ts
@@ -1,11 +1,5 @@
 const defaultConfig = {
   PORT: process.env.PORT || 3000,
-  apiKey: "AIzaSyAkpJiNiJ8PX18ETSBfiidnA_FZIKXazbU",
-  authDomain: "behemoth-1f3a5.firebaseapp.com",
-  databaseURL: "https://behemoth-1f3a5.firebaseio.com",
-  projectId: "behemoth-1f3a5",
-  storageBucket: "behemoth-1f3a5.appspot.com",
-  messagingSenderId: "1023358943709"
 }
 
 const config = {
--- src/modules/firebase/index.ts
+++ src/modules/firebase/index.ts
@@ -1,21 +1,40 @@
 import * as firebase from 'firebase'
+import * as admin from 'firebase-admin';
 
 import { map } from 'lodash'
+require('dotenv').config()
 
-import constants from '../../../config/constants'
+const serviceAccount = {
+  type: process.env.type,
+  project_id: process.env.project_id,
+  private_key_id: process.env.private_key_id,
+  private_key: process.env.private_key,
+  client_email: process.env.client_email,
+  client_id: process.env.client_id,
+  auth_uri: process.env.auth_uri,
+  token_uri: process.env.token_uri,
+  auth_provider_x509_cert_url: process.env.auth_provider_x509_cert_url,
+  client_x509_cert_url: process.env.client_x509_cert_url
+}
 
-firebase.initializeApp({
-    apiKey: constants.apiKey,
-    authDomain: constants.authDomain,
-    databaseURL: constants.databaseURL,
-    projectId: constants.projectId,
-    storageBucket: constants.storageBucket,
-    messagingSenderId: constants.messagingSenderId
+
+admin.initializeApp({
+  //@ts-ignore
+  credential: admin.credential.cert(serviceAccount),
+  //@ts-ignore
+  apiKey: process.env.APIKEY,
+  authDomain: process.env.AUTHDOMAIN,
+  databaseURL: process.env.DATABASEURL,
+  projectId: process.env.PROJECTID,
+  storageBucket: process.env.STORAGEBUCKET,
+  messagingSenderId: process.env.MESSAGINGSENDERID
 })
+
+
 
 const mapSnapshotToEntity = snapshot => ({ id: snapshot.key, ...snapshot.val() })
 const mapSnapshotToEntities = snapshot => map(snapshot.val(), (value, id) => ({ id, ...value }))
-const ref = path => firebase.database().ref(path)
+const ref = path => admin.database().ref(path)
 const getValue = path => ref(path).once('value')
 const getEntity = path => getValue(path).then(mapSnapshotToEntity)
 const getEntities = path => getValue(path).then(mapSnapshotToEntities)
