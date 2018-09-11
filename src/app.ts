ae086ca1788066405d53339c45648feab98756ed
= <>
2018-09-11 17:32:15 +0300

    app.ts updated

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,9 @@ import * as express from 'express'
 import * as bodyParser from 'body-parser'
 
 import constants from '../config/constants'
-import routes from './modules'
 import { server } from '../src/modules/graphql'
 
 const app = express()
-
-app.use('/api/v1/', routes)
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true}))
@@ -16,8 +13,9 @@ if(process.env.NODE_ENV === 'development') {
   const morgan = require('morgan')
   app.use(morgan('dev'))
 }
+const path = '/api/graphql'
 
-server.applyMiddleware({app})
+server.applyMiddleware({app, path})
 
 app.listen(constants.PORT, (err) => {
   if(err) {
