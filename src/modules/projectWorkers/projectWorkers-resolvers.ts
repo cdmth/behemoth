b5633f3e5afb165d32b7bf5420a024a5511ef812
antti pitkänen <pitkan@gmail.com>
2018-09-18 10:01:12 +0300

    dont save projectId or workerId

--- src/modules/projectWorkers/projectWorkers-resolvers.ts
+++ src/modules/projectWorkers/projectWorkers-resolvers.ts
@@ -1,28 +1,23 @@
-import { getWorkersByProjectId, setChildEntity, deleteEntity} from '../firebase'
+import { getWorkersByProjectId, setChildEntity, deleteEntity } from '../firebase'
 import { pubsub } from '../firebase/pubsubber'
 
-const path : string = 'projectWorkers'
+const path: string = 'projectWorkers'
 
 const projectWorkersResolvers = {
     Query: {
-        getWorkersByProjectId: (_, { projectId } : { projectId : string }) => getWorkersByProjectId(path, projectId),
-    },
-    ProjectWorkers: {
-        workers: (fuck) => {
-           return fuck
-        }
+        getWorkersByProjectId: (_, { projectId }: { projectId: string }) => getWorkersByProjectId(path, projectId),
     },
     Mutation: {
         addWorkerToProject: (_, args) => {
-            const { projectId, workerId } = args
-            return setChildEntity(path + '/' + projectId, workerId, args)
+            const { projectId, workerId, ...rest } = args
+            return setChildEntity(path + '/' + projectId, workerId, rest)
         },
-        removeWorkerFromProject: async (_, { workerId, projectId } : { workerId: string, projectId: string }) => {
+        removeWorkerFromProject: async (_, { workerId, projectId }: { workerId: string, projectId: string }) => {
             try {
                 await deleteEntity(path + '/' + projectId, workerId)
                 return {
                     message: 'Someone got fired'
-                } 
+                }
             } catch (err) {
                 throw new Error(err)
             }
